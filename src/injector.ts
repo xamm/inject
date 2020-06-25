ee011a24308a8be3c508d671b36721750b9c2707
xamm <39380924+xamm@users.noreply.github.com>
2020-06-25 20:55:32 +0200

    publish from root dir

--- src/injector.ts
+++ src/injector.ts
@@ -1,37 +1,54 @@
 import { Dictionary, IDictionaryEntry } from "modern-dictionary";
+
 export namespace Injector {
 
-  const moduleDictionary = new Dictionary();
+    const moduleDictionary = new Dictionary();
 
-  export function setInstance(name: string, instance: any) {
-    validateNameParameter(name);
-    if (!instance) {
-      throw new Error("Instance parameter must not be null or undefined.");
+    /**
+     * @description Sets an instance which should be injected / mocked by name.
+     * @export
+     * @param {string} name The name by which the injected / mocked object should be retrieved through import("name") .
+     * @param {*} instance The object which is used to mock / inject.
+     */
+    export function setInstance(name: string, instance: any) {
+        validateNameParameter(name);
+        if (!instance) {
+            throw new Error("Instance parameter must not be null or undefined.");
+        }
+        moduleDictionary.add(name, instance);
     }
-    moduleDictionary.add(name, instance);
-  }
 
-  const validateNameParameter: Function = (name: string): void => {
-    if (!name) {
-      throw new Error("Name parameter must not be null, undefined or empty string.");
+    const validateNameParameter: Function = (name: string): void => {
+        if (!name) {
+            throw new Error("Name parameter must not be null, undefined or empty string.");
+        }
+    };
+
+    /**
+     * @description Removes the injected / mocked instance.
+     * @export
+     * @param {string} name The name by which the injected / mocked object is identified.
+     */
+    export function clear(name: string) {
+        validateNameParameter(name);
+        moduleDictionary.remove(name);
     }
-  };
 
-  export function clear(name: string) {
-    validateNameParameter(name);
-    moduleDictionary.remove(name);
-  }
+    /**
+     * @description Returns the injected / mocked instance by name or the default if not present.
+     * @export
+     * @param {string} name The name by which the injected / mocked object is identified.
+     * @returns {Promise<any>} The promise which needs to be resolved in order to access the injected objects members.
+     */
+    export async function Instance(name: string): Promise<any> {
+        validateNameParameter(name);
 
-  export async function Instance(name: string): Promise<any> {
-    validateNameParameter(name);
+        const instance: IDictionaryEntry | null = moduleDictionary.tryGetEntry(name);
 
-    const instance: IDictionaryEntry | null = moduleDictionary.tryGetEntry(name);
+        if (instance) return instance.value;
 
-    if (instance) {
-      return instance.value;
+        const newInstance: any = await import(name);
+        moduleDictionary.forceAdd(name, newInstance);
+        return newInstance;
     }
-    const newInstance: any = await import(name);
-    moduleDictionary.forceAdd(name, newInstance);
-    return newInstance;
-  }
 }
